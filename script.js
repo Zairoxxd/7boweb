1c8ebe781cf48a60d2c330ceccb1295d11408a0c
zax <raizox67@gmail.com>
2025-04-06 21:59:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.getElementById('config-icon').addEventListener('click', function() {
+    document.getElementById('config-panel').classList.toggle('active');
+});
 
+document.getElementById('close-panel').addEventListener('click', function() {
+    document.getElementById('config-panel').classList.remove('active');
+});
